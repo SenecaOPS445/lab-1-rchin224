fdf37f8e869f32072281f24c1289c93b433c93b1
rchin224 <168853009+rchin224@users.noreply.github.com>
2025-01-10 11:57:09 -0500

    Update lab1c.py

--- lab1c.py
+++ lab1c.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
-num1 = 5
-num2 = 10
-#print (num1) 
-#print (num2)
-sum = num1 + num2
-#print (sum)
-#print ('The sum is: ' + str(sum))
-
-#!/usr/bin/env python3
 
 # Define the required objects
 name = "Isaac"
