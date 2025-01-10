845e7a1b1874ab1c489b3a4883c2a036d9aa57d9
rchin224 <168853009+rchin224@users.noreply.github.com>
2025-01-10 12:06:52 -0500

    Update lab1a.py

--- lab1a.py
+++ lab1a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''Description: This program will output "hello world" to the screen.'''
 
+
 # Any line that starts with a "#" is also known as a comment,
 # these lines are ignored by the python interpreter even if
 # they contain code. The very first line is called a Shebang line, 
